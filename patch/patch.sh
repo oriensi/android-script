d2d8e21fd8341a92074a34388bfaf648999634d9
pasiel <fxdgrb@gmail.com>
2019-04-16 20:43:34 +0800

    合入patch

---
+++ patch/patch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ 1 = ${#1} ]]; then
+    OPERATOR=$1
+    PATCH_DIR=$2
+else
+    echo "./patch <operator> <patch/path>"
+    echo " <operator> c check, a apply, f am"
+    exit
+fi
+
+for i in ` find $PATCH_DIR -type f -name "*.patch" | sort `; do
+    TEMP_DIR=$i
+    TEMP_DIR=`echo $TEMP_DIR | sed "s|$PATCH_DIR\(.*/\)[^/]*.patch$|\1|"`
+    if [[ $OPERATOR = "c" ]]; then
+        echo "git apply --check --directory=$TEMP_DIR $i"
+        git apply --check --directory=$TEMP_DIR $i
+    elif [[ $OPERATOR = "a" ]]; then
+        echo "git apply --directory=$TEMP_DIR $i"
+        git apply --directory=$TEMP_DIR $i
+    elif [[ $OPERATOR = "f" ]]; then
+        echo "git am --directory=$TEMP_DIR $i"
+        git am --directory=$TEMP_DIR $i
+    fi
+    sleep 1s
+done
+
